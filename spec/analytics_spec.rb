ea2b6d4c8429f965bf3189fe60e24139fc8cc25f
Travis Dempsey <dempsey.travis@gmail.com>
2016-04-12 18:01:34 -0500

    updates to analytics specs

--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -1,9 +1,33 @@
 require 'spec_helper'
 
-describe 'analytics', nodespec: { 'user' => 'trdempsey', 'adapter' => 'ssh' } do
+describe 'localhost', nodespec: { 'user' => 'trdempsey', 'adapter' => 'ssh' } do
   it_behaves_like 'cloud formation machine'
 
+  context command('mongo --quiet --host 10.234.192.80 --eval "db.getMongo().getDBNames()"') do
+    its(:stdout){ is_expected.to match /bank_verification/ }
+  end
+
   context service('celery_analytics'), sudo: true do
-    it { should be_running.under('supervisor') }
+    it { is_expected.to be_running.under('supervisor') }
+  end
+
+  context file('/data') do
+    it { is_expected.to be_mounted }
+  end
+
+  context file('/app') do
+    it { is_expected.to be_mounted }
+  end
+
+  context file('/var/log/venmo_platform') do
+    it { is_expected.to be_mounted }
+  end
+
+  context file('/app/venmo-data-science/current') do
+    it { is_expected.to be_directory }
+  end
+
+  context file('/app/venmo_platform/current') do
+    it { is_expected.to be_directory }
   end
 end
--- spec/shared_examples/cloud_formation_machine.rb
+++ spec/shared_examples/cloud_formation_machine.rb
@@ -1,17 +1,33 @@
 shared_examples 'cloud formation machine', cloud_formation: true do
   context command('ruby --version') do
-    its(:stdout){ should match /2.0/ }
+    its(:stdout){ is_expected.to match /2.0/ }
   end
 
   context command('puppet --version') do
-    its(:stdout){ should match /3.8.4/ }
+    its(:stdout){ is_expected.to match /3.8.4/ }
   end
 
   context service('amazon-ssm-agent') do
-    it{ should be_running }
+    it { is_expected.to be_running }
   end
 
   context service('codedeploy-agent') do
-    it{ should be_running }
+    it { is_expected.to be_running }
+  end
+
+  context file('/tmp') do
+    it { is_expected.to be_mounted }
+  end
+
+  context file('/var') do
+    it { is_expected.to be_mounted }
+  end
+
+  context file('/var/log/audit') do
+    it { is_expected.to be_mounted }
+  end
+
+  context file('/home') do
+    it { is_expected.to be_mounted }
   end
 end
